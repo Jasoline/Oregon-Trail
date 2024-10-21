f11b463a39939cd5ae0fc42eec6bc99085354c07
David Tran <davdtrann@gmail.com>
2024-10-20 21:38:26 -0400

    fixed button not disapearing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import pygame_widgets
-from buttons import create_credit_message, create_play_button, create_quit_button, create_back_button, create_selection_message, create_name_inputs, create_name_num
+from buttons import *
 
 def start():
     # Initialize Pygame
@@ -23,6 +23,8 @@ def start():
 
     # Screen helper
     screen_helper = {'screen': 'title'}
+    prev_screen = []
+    
 
     # Quit button helper
     def close_window():
@@ -32,16 +34,26 @@ def start():
     # Play button helper
     def click_play():
         screen_helper['screen'] = 'char_select'
+        prev_screen.append('title')
 
     # Back button helper
     def click_back():
-        screen_helper['screen'] = 'title'
+        screen_helper['screen'] = prev_screen.pop()
+    
+    def click_next():
+        if screen_helper['screen'] == 'char_select':
+            prev_screen.append('char_select')            
+            screen_helper['screen'] = 'month_select'
+        
+        
+
 
     # Import button functionality
     credit_message = create_credit_message(screen)
     play_button = create_play_button(screen, click_play)
     quit_button = create_quit_button(screen, close_window)
     back_button = create_back_button(screen, click_back)
+    next_button = create_next_button(screen, click_next)
     selection_message = create_selection_message(screen)
     name_inputs = create_name_inputs(screen)
     name_nums = create_name_num(screen)
@@ -57,30 +69,56 @@ def start():
 
         # Render title screen
         if screen_helper['screen'] == 'title':
+            screen.fill((0, 0, 0))
             screen.blit(bg_title, (0, 0))
 
             # Draw the buttons
             play_button.listen(events)
+            play_button.show()
             play_button.draw()
             quit_button.listen(events)
             quit_button.draw()
+            quit_button.show()
+            back_button.hide()            
+            next_button.hide()
+
+            
             credit_message.draw()
 
         # Render character selection screen
         elif screen_helper['screen'] == 'char_select':
+            screen.fill((0, 0, 0))
             screen.blit(bg_char, (0, 0))
 
             # Draw the buttons
-            back_button.listen(events)
+            play_button.hide()
+            quit_button.hide()
+            back_button.show()
             back_button.draw()
+            next_button.show()
+            next_button.draw()
+            
+
             selection_message.draw()
             for num in name_nums:
                 num.draw()
 
             # Draw input boxes
             for input_box in name_inputs:
-                input_box.listen(events)
+                
                 input_box.draw()
+
+        elif screen_helper['screen'] == 'month_select':
+            
+            screen.fill((0, 0, 0))
+            
+
+            # Draw the buttons
+            
+            next_button.draw()
+            
+            back_button.draw()
+            
 
         # Update the display
         pygame.display.flip()
