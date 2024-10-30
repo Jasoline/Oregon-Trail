fb0f9893751facd254e6be788ef7d31437baefd5
David Tran <davdtrann@gmail.com>
2024-10-29 20:44:46 -0400

    Use properties for variables

--- stats.py
+++ stats.py
@@ -3,10 +3,10 @@ class Stats:
         # Initialize instance variables
         self.__distance_travelled = 0  # Distance travelled by the party
         self.__days_passed = 0  # Number of days passed
-        self.__day = 1,  # Current day of the month
-        self.__month = 0  # Current month
-        self.__month_names = ["January", "February", "March", "April", "May", "June", "July", "August", "September",
-                              "October", "November", "December"]
+        self.__day = 1  # Current day of the month
+        self.__month = 1  # Current month
+        self.__month_names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        self.__year = 1848  # Current year
         self.__party_health = 100  # Health of the party
         self.__events_occurred = 0  # Number of events occurred
         self.__deaths = 0  # Number of deaths
@@ -16,10 +16,27 @@ class Stats:
         self.__oxen = 0  # Amount of oxen
         self.__clothing = 0  # Amount of clothing
         self.__wagon_health = 100  # Health of the wagon
+        self.__spare_parts = 0  # Amount of spare parts
+    @property
+    def year(self):
+        # Return the current year
+        return self.__year
+    @year.setter
+    def year(self, year):
+        # Set the current year
+        self.__year = year
+    @property
+    def spare_parts(self):
+        # Return the amount of spare parts
+        return self.__spare_parts
+    @spare_parts.setter
+    def spare_parts(self, spare_parts):
+        # Set the amount of spare parts
+            self.__spare_parts = spare_parts
     @property
     def month_name(self):
         # Return the list of month names
-        return self.__month_names[self.__month - 1]
+        return self.__month_names[(self.__month - 1) % 12]
     @property
     def ammo(self):
         # Return the amount of ammunition
@@ -27,7 +44,7 @@ class Stats:
     @ammo.setter
     def ammo(self, ammo):
         # Set the amount of ammunition
-            self.__ammo += ammo
+            self.__ammo = ammo
     @property
     def food(self):
         # Return the amount of food
@@ -35,7 +52,7 @@ class Stats:
     @food.setter
     def food(self, food):
         # Set the amount of food
-            self.__food += food
+            self.__food = food
     @property
     def clothing(self):
         # Return the amount of clothing
@@ -43,7 +60,7 @@ class Stats:
     @clothing.setter
     def clothing(self, clothing):
         # Set the amount of clothing
-            self.__clothing += clothing
+            self.__clothing = clothing
     @property
     def oxen(self):
         # Return the amount of oxen
@@ -51,7 +68,7 @@ class Stats:
     @oxen.setter
     def oxen(self, oxen):
         # Set the amount of oxen
-            self.__oxen += oxen
+            self.__oxen = oxen
     @property
     def days_passed(self):
         # Return the number of days passed
@@ -60,7 +77,7 @@ class Stats:
     @days_passed.setter
     def days_passed(self, days):
         # Set the number of days passed
-        self.__days_passed += days
+        self.__days_passed = days
 
     @property
     def distance_travelled(self):
@@ -80,7 +97,22 @@ class Stats:
     @day.setter
     def day(self, day):
         # Set the current day of the month
-        self.__day = day
+        if day == 32 and (self.__month % 12 == 1 or self.__month % 12 == 3 or self.__month % 12 == 5 or self.__month % 12 == 7 or self.__month % 12 == 8 or self.__month % 12 == 10 or self.__month % 12 == 0):
+            if self.__month % 12 == 0:
+                self.__year += 1
+            self.__day = 1
+            self.__month += 1
+        elif day == 31 and (self.__month % 12 == 4 or self.__month % 12 == 6 or self.__month % 12 == 9 or self.__month % 12 == 11):
+            
+            self.__day = 1
+            self.__month += 1
+        elif day == 29 and self.__month % 12 == 2:
+            
+            self.__day = 1
+            self.__month += 1
+        else:
+            self.__day = day
+        
 
     @property
     def month(self):
@@ -100,7 +132,7 @@ class Stats:
     @party_health.setter
     def party_health(self, health):
         # Set the health of the party
-        self.__party_health += health
+        self.__party_health = health
 
     @property
     def events_occurred(self):
@@ -130,7 +162,7 @@ class Stats:
     @money.setter
     def money(self, money):
         # Set the amount of money
-            self.__money += money
+            self.__money = money
 
     @property
     def supplies(self):
@@ -145,28 +177,28 @@ class Stats:
     @wagon_health.setter
     def wagon_health(self, health):
         # Set the health of the wagon
-            self.__wagon_health += health
+            self.__wagon_health = health
         
 
     def update_death(self):
         # Update the number of deaths
-        self.__deaths += 1
+        self.__deaths = 1
 
     def increase_day(self):
         # Increase the current day of the month
         if self.__month == 1 or self.__month == 3 or self.__month == 5 or self.__month == 7 or self.__month == 8 or self.__month == 10 or self.__month == 12:
             if self.__day == 31:
                 self.__day = 1
-                self.__month += 1
+                self.__month = 1
         elif self.__month == 4 or self.__month == 6 or self.__month == 9 or self.__month == 11:
             if self.__day == 30:
                 self.__day = 1
-                self.__month += 1
+                self.__month = 1
         elif self.__month == 2:
             if self.__day == 28:
                 self.__day = 1
-                self.__month += 1
+                self.__month = 1
         else:
-            self.__day += 1
+            self.__day = 1
 
 
