30c05dc7d2d07da41c5335afba665aca0628b083
David Tran <davdtrann@gmail.com>
2024-10-29 23:10:10 -0400

    Display details about amount

--- randomizer.py
+++ randomizer.py
@@ -19,7 +19,7 @@ class FoodPoisoning:
     def execute(self):
         damage = random.randint(-5, -1)
         self.stats.party_health += damage
-        return f"You got food poisoning, your health is now: {self.stats.party_health}"
+        return f"You got food poisoning, you took {-1*damage} damage. Your health is now: {self.stats.party_health}"
 
 
 class Theft:
@@ -29,7 +29,7 @@ class Theft:
     def execute(self):
         loss = random.randint(-9, -3)
         self.stats.money += loss
-        return f"You got robbed, your money is now: {self.stats.money}"
+        return f"You got robbed, you lost {-1*loss} money. your money is now: {self.stats.money}"
 
 
 class Dysentery:
@@ -39,7 +39,7 @@ class Dysentery:
     def execute(self):
         damage = random.randint(-10, -5)
         self.stats.party_health += damage
-        return f"You got dysentery! Your health is now: {self.stats.party_health}"
+        return f"You got dysentery! You took {-1*damage} damage. Your health is now: {self.stats.party_health}"
 
 
 class WagonDmg:
@@ -49,7 +49,7 @@ class WagonDmg:
     def execute(self):
         damage = random.randint(-7, -2)
         self.stats.wagon_health += damage
-        return f"Your wagon took damage from poor road conditions, your wagon's health is now: {self.stats.wagon_health}"
+        return f"Your wagon took damage from poor road conditions, your wagon took {-1*damage} damage. Your wagon's health is now: {self.stats.wagon_health}"
 
 
 class Loot:
@@ -59,14 +59,14 @@ class Loot:
     def execute(self):
         gold_found = random.randint(1, 10)
         self.stats.money += gold_found
-        return f"You found a pouch, your money is now: {self.stats.money}"
+        return f"You found a pouch, you found {gold_found} money. Your money is now: {self.stats.money}"
 
 
 # Function to handle events
 def events_occurred(stats):
-    pygame.mixer.music.unload()
-    pygame.mixer.music.load(os.path.join("songs", "The Long Road [ ezmp3.cc ].mp3"))
-    pygame.mixer.music.play(-1)
+    # pygame.mixer.music.unload()
+    # pygame.mixer.music.load(os.path.join("songs", "The Long Road [ ezmp3.cc ].mp3"))
+    # pygame.mixer.music.play(-1)
     event_number = random.randint(1, 5) 
     if event_number == 1:
         return FoodPoisoning(stats).execute()
@@ -81,8 +81,8 @@ def events_occurred(stats):
 
 
 # code to test events
-if __name__ == "__main__":
-    stats_instance = Stats()  # Create an instance of Stats -- probs not a thing for final result
-    print(events_occurred(stats_instance))  # Print the result of a random event
+# if __name__ == "__main__":
+    # stats_instance = Stats()  # Create an instance of Stats -- probs not a thing for final result
+    # print(events_occurred(stats_instance))  # Print the result of a random event
 
 
